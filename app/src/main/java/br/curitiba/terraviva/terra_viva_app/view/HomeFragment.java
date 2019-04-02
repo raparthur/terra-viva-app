1a0f2885e2f87524d1a316a577e4ab4681d16e71
raparthur <raparthur@gmail.com>
2019-04-01 22:38:52 -0300

    dropdown subcategoria incompleto

--- app/src/main/java/br/curitiba/terraviva/terra_viva_app/view/HomeFragment.java
+++ app/src/main/java/br/curitiba/terraviva/terra_viva_app/view/HomeFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import br.curitiba.terraviva.terra_viva_app.R;
 import br.curitiba.terraviva.terra_viva_app.connexion.JSONManager;
@@ -26,6 +29,24 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);//Inflate Layout
         JSONManager manager = new JSONManager(getContext(), getActivity(), view);
+
+        Spinner dropdown = view.findViewById(R.id.dropdown);
+        TextView tv_subcateg = view.findViewById(R.id.tv_subcateg);
+
+        dropdown.setVisibility(View.GONE);
+        tv_subcateg.setVisibility(View.GONE);
+
+        //create a list of items for the spinner.
+        String[] items = new String[]{"TODAS"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_checked, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        //show
+        dropdown.setVisibility(View.VISIBLE);
+        tv_subcateg.setVisibility(View.VISIBLE);
 
         manager.getCategorias();
 
